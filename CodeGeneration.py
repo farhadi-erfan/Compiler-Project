a3ba6ba4d2ed349ddb2c5ee863809cb4037be5f9
Sina Rismanchian <sina.rismanchian@gmail.com>
2019-06-12 11:56:23 +0430

    some develop. not finalized on expression.

--- CodeGeneration.py
+++ CodeGeneration.py
@@ -11,4 +11,19 @@ class CodeGenerator:
         self.data_index = 100
         self.temp_index = 500
 
+    def get_address_by_token(self, label):
+        for symcell in self.symbol_table.stack:
+            if symcell['token'] == label:
+                return symcell['addr']
+        raise Exception('Token not valid!')
 
+    def get_temp(self):
+        res = self.temp_index
+        self.temp_index += 4
+        return res
+
+فرچم
+بعد ۱۲ تا تونل
+۳ راهی
+سمت راست
+خلخال
--- SemanticRoutines.py
+++ SemanticRoutines.py
@@ -6,16 +6,19 @@ class SemanticRoutines:
     def plast(self, cg, key):
         cg.ss.push(key)
 
+    @staticmethod
+    def assign(cg, addr_from, addr_to):
+        cg.pb[cg.index] = '(ASSIGN, {}, {})'.format(addr_from, addr_to)
+        cg.index += 4
+
     def vardec(self, cg):
         identifier_token = cg.ss.top()
 
         if identifier_token == ';':
-            # paddr
             cg.symbol_table.push({
                 'token': cg.ss.get_from_top(1),
                 'type': cg.ss.get_from_top(2),
                 'addr': cg.ss.data_index,
-                'ref': True
             })
             cg.data_index += 4
             cg.ss.pop(3)
@@ -26,7 +29,16 @@ class SemanticRoutines:
                 'type': cg.ss.get_from_top(3),
                 'addr': cg.ss.data_index
             })
+            self.assign(cg, '#{}'.format(cg.data_index + 4), '{}'.format(cg.data_index))
             arr_size = cg.ss.get_from_top(1)
-            cg.data_index += 4 * arr_size
+            cg.data_index += 4 * (arr_size + 1)
+            cg.ss.pop(4)
+
         elif identifier_token == ')':
-            pass # TODO: function dec
+            pass    # TODO: function dec
+
+    def null(self, cg):
+        cg.ss.push('NaK') # not an important key
+
+    def paddr(self, cg, id):
+        cg.ss.push(cg.get_address_by_token(id))
